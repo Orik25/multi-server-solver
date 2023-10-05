dc9fdb6fda8b0129eccbfe78328aac57d26d88ca
Orik25 <orestpilavka@gmail.com>
2023-10-05 17:49:08 +0300

    added exceptions in services

--- application-server/src/main/java/com/orik/applicationserver/service/impl/RoleServiceImpl.java
+++ application-server/src/main/java/com/orik/applicationserver/service/impl/RoleServiceImpl.java
@@ -2,6 +2,7 @@ package com.orik.applicationserver.service.impl;
 
 import com.orik.applicationserver.DAO.RoleRepository;
 import com.orik.applicationserver.entities.Role;
+import com.orik.applicationserver.exception.NoRoleFoundException;
 import com.orik.applicationserver.service.interfaces.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,13 +19,13 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public Role findByName(String roleName) {
-        return roleRepository.findByName(roleName) //todo: add custom exception
-                .orElseThrow(NullPointerException::new);
+        return roleRepository.findByName(roleName)
+                .orElseThrow(()-> new NoRoleFoundException("No role found with this name: " + roleName));
     }
 
     @Override
     public Role findById(Long id) {
-        return roleRepository.findById(id) //todo: add custom exception
-                .orElseThrow(NullPointerException::new);
+        return roleRepository.findById(id)
+                .orElseThrow(()->new NoRoleFoundException("No role found with this id: " + id));
     }
 }
