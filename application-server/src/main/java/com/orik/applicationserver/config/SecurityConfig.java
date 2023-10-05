5074e8b1d6c4e645084223497dab04573f092c23
Orik25 <orestpilavka@gmail.com>
2023-10-05 18:29:03 +0300

    added login logic with correct security

--- application-server/src/main/java/com/orik/applicationserver/config/SecurityConfig.java
+++ application-server/src/main/java/com/orik/applicationserver/config/SecurityConfig.java
@@ -1,9 +1,20 @@
 package com.orik.applicationserver.config;
 
+import com.orik.applicationserver.constant.RoleData;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
@@ -11,5 +22,50 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private static final String ADMIN = RoleData.ADMIN.getRoleName();
+    private static final String USER = RoleData.USER.getRoleName();
+    private static final String VIP = RoleData.VIP.getRoleName();
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/login", "/registration").permitAll()
+                        .requestMatchers("/system/**").hasRole(ADMIN)
+                        .requestMatchers("/api/**").hasAnyRole(VIP, USER)
+                )
+                .formLogin((form) -> form
+//                        .loginPage("/login")
+                        .successHandler(successHandler())
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll)
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        Map<String, String> roleRedirectMap = createRoleRedirectMap();
+        return (request, response, authentication) -> {
+            for (GrantedAuthority auth : authentication.getAuthorities()) {
+                String authority = auth.getAuthority();
+                if (roleRedirectMap.containsKey(authority)) {
+                    response.sendRedirect(roleRedirectMap.get(authority));
+                    return;
+                }
+            }
+        };
+    }
+
+    private Map<String, String> createRoleRedirectMap() {
+        Map<String, String> roleRedirectMap = new HashMap<>();
+        roleRedirectMap.put(RoleData.ADMIN.getDBRoleName(), "/system");
+        roleRedirectMap.put(RoleData.USER.getDBRoleName(), "/api");
+        roleRedirectMap.put(RoleData.VIP.getDBRoleName(), "/api");
+        return roleRedirectMap;
     }
 }
---
+++ application-server/src/main/java/com/orik/applicationserver/security/CustomAuthenticationProvider.java
@@ -0,0 +1,49 @@
+package com.orik.applicationserver.security;
+
+import com.orik.applicationserver.exception.CustomAuthenticationException;
+import com.orik.applicationserver.exception.NoUserFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private CustomUserDetailsService userDetailsService;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public CustomAuthenticationProvider(CustomUserDetailsService userDetailsService,BCryptPasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws CustomAuthenticationException {
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+
+        try {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+            checkPassword(password,userDetails);
+            return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+        } catch (NoUserFoundException e) {
+            throw new CustomAuthenticationException("Authentication failed: " + e.getMessage());
+        }
+    }
+    private void checkPassword(String password,UserDetails userDetails){
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new CustomAuthenticationException("Authentication failed: Invalid password");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
