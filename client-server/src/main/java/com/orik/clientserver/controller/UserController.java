1966c73af468f231537a7c5c2cc02c6f189b9edf
Orik25 <orestpilavka@gmail.com>
2023-10-09 13:20:08 +0300

    added user-page for history

--- client-server/src/main/java/com/orik/clientserver/controller/UserController.java
+++ client-server/src/main/java/com/orik/clientserver/controller/UserController.java
@@ -1,15 +1,38 @@
 package com.orik.clientserver.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.orik.clientserver.service.interfaces.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class UserController {
 
+    private final RequestService requestService;
+
+    @Autowired
+    public UserController(RequestService requestService) {
+        this.requestService = requestService;
+    }
+
+
     @GetMapping
-    public String showUserPage(){
-        return "api";
+    public String getUsers(Model model,
+                           @RequestParam(name = "page", defaultValue = "0") int page,
+                           @RequestParam(name = "size", defaultValue = "1") int size,
+                           @RequestParam(name = "sortField", defaultValue = "id") String sortField,
+                           @RequestParam(name = "sortOrder", defaultValue = "asc") String sortOrder) {
+        model.addAttribute("historyPage", requestService.getAllRequestsSorted(page,size,sortField, sortOrder));
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortOrder", sortOrder);
+        return "user/user-page";
+    }
+
+    @GetMapping("/delete-request/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        requestService.deleteById(id);
+        return "redirect:/api";
     }
 }
