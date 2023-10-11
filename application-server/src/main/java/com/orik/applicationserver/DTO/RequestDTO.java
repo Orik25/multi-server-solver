b075f606843fc7c973db0ac743bbca5738c2ad00
Orik25 <orestpilavka@gmail.com>
2023-10-11 17:33:57 +0300

    skip file

--- application-server/src/main/java/com/orik/applicationserver/DTO/RequestDTO.java
+++ application-server/src/main/java/com/orik/applicationserver/DTO/RequestDTO.java
@@ -31,13 +31,6 @@ public class RequestDTO {
         this.request = request;
     }
 
-    public Long getResult() {
-        return result;
-    }
-
-    public void setResult(Long result) {
-        this.result = result;
-    }
 
     public String getStatus() {
         return status;
@@ -47,24 +40,17 @@ public class RequestDTO {
         this.status = status;
     }
 
-    public Integer getPort() {
-        return port;
-    }
-
-    public void setPort(Integer port) {
-        this.port = port;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RequestDTO that = (RequestDTO) o;
-        return Objects.equals(id, that.id) && Objects.equals(request, that.request) && Objects.equals(result, that.result) && Objects.equals(status, that.status) && Objects.equals(port, that.port);
+        return Objects.equals(id, that.id) && Objects.equals(request, that.request) && Objects.equals(status, that.status);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, request, result, status, port);
+        return Objects.hash(id, request, status);
     }
 }
